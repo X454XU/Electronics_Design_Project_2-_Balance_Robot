8a0748c02908f09048794154b2611ce51653f6f4
Liamxu423 <151572498+Liamxu423@users.noreply.github.com>
2024-05-30 02:11:08 +0100

    Update main.cpp

--- esp32-starter/src/main.cpp
+++ esp32-starter/src/main.cpp
@@ -127,9 +127,15 @@ void loop()
   if (millis() > printTimer) {
     printTimer += PRINT_INTERVAL;
     Serial.print(tiltx*1000);
+    Serial.print(tiltx*180/PI)
+    //print tilt angle in degrees
     Serial.print(' ');
     Serial.print(step1.getSpeedRad());
     Serial.println();
+    Serial.print(step2.getSpeedRad());
+    //print step2 speed
+    Serial.println(setpoint)；
+    //print setpoint
 
   }
 
