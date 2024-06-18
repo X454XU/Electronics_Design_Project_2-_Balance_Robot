b28bec08ad84bb3f0d6a66323f303d7675a6fcaa
Maximilian Adam <maxjadam@icloud.com>
2024-06-18 11:21:27 +0100

    cleanup

--- esp32-starter/.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-c3-devkitc-02/FrameworkArduino/USB.cpp.o: \
- /Users/seb/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /Users/seb/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/seb/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- esp32-starter/.pio/build/esp32-c3-devkitc-02/src/PIDController.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32-c3-devkitc-02/src/PIDController.cpp.o: \
- src/PIDController.cpp include/PIDController.h
